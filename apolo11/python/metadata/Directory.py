6f409ee211a053781d1bd40f442ce582bf9666dc
Julian local <juliansernavelez@gmail.com>
2024-01-17 14:55:36 -0500

    Agregando docstring en directory.py

--- apolo11/python/metadata/Directory.py
+++ apolo11/python/metadata/Directory.py
@@ -2,22 +2,26 @@ import os
 
 
 class CurrentDirectory:
-    '''Una clase que representa el directorio de trabajo actual.'''
+    """Class that represents the "actual" work directory
+    """
     current_directory: str = os.getcwd()
 
     
 class PathDataRaw:
-    '''Una clase que representa el directorio data.'''
+    """Class that represents the "data" work directory
+    """
     data_directory: str = os.path.join(CurrentDirectory.current_directory, 'data')
 
 
 class PathInputRaw:
-    '''Una clase que representa el directorio input.'''
+    """Class that represents the "input" work directory
+    """
     input_directory: str = os.path.join(CurrentDirectory.current_directory, 'input')
     configuration_file: str = os.path.join(input_directory, 'configuration.yaml')
 
 
 class PathOutputRaw:
-    '''Una clase que representa el directorio output.'''
+    """Class that represents the "output" work directory
+    """
     output_directory: str = os.path.join(CurrentDirectory.current_directory, 'output')
     output_files: str = os.path.join(output_directory, 'devices')
